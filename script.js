006e8320b286ac8333d081209b8384a288c4e9b0
salobando <salobando2023@gmail.com>
2025-11-27 10:57:39 -0500

    subiendo html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+flatpickr("#calendario", {
+    dateFormat: "Y-m-d",
+    minDate: "today"
+});
+const calendarElementList = Array.prototype.slice.call(document.querySelectorAll('.calendar'))
+const calendarList = calendarElementList.map(calendarEl => {
+  return new coreui.Calendar(calendarEl)
+})
